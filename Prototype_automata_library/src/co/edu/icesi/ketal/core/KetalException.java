954640de7378614cfa4f4c850e13ef379884fa74
Oscar Kiyoshige Garces Aparicio <oscarkiyoshigegarcesaparicio@MacBook-Pro-de-Oscar-2.local>
2012-11-22 21:50:51 -0500

    Commits Faltantes.

---
+++ Prototype_automata_library/src/co/edu/icesi/ketal/core/KetalException.java
@@ -0,0 +1,12 @@
+package co.edu.icesi.ketal.core;
+
+public class KetalException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 10101010101L;
+
+	public KetalException(String msg) {
+        super(msg);
+    }
+}
---
+++ Prototype_automata_library/src/co/edu/icesi/ketal/test/causal/StackOperatorsDemo.java
@@ -0,0 +1,16 @@
+package co.edu.icesi.ketal.test.causal;
+
+/**
+ * 
+ * This demo shows the functionality of the operators DefaultEqualsExpression and 
+ * ConcurrentEqualsExpression, in two different scenarios:
+ * 
+ * 1. The capability of detect the determinated causality relationship
+ * 2. Detect a specific pattern of causality with a order set of events.
+ * 
+ * @author oscarkiyoshigegarcesaparicio
+ *
+ */
+public class StackOperatorsDemo {
+
+}
