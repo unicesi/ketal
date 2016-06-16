56388f7a3b9020e4e5526bbbbfcd9e2b8f6f1b6e
camilo040494 <camilo040494@gmail.com>
2016-06-16 17:49:27 -0500

    Cleaned of syso's calls
    Removed the syso in the tearDown method

--- src/test/java/co/edu/icesi/ketal/test/distribution/DistributionTest.java
+++ src/test/java/co/edu/icesi/ketal/test/distribution/DistributionTest.java
@@ -48,7 +48,6 @@ public class DistributionTest {
 
 	@After
 	public void tearDown() throws Exception {
-		System.out.println("termino");
 		eventBroker1.closeComunication();
 		eventBroker2.closeComunication();
 	}
